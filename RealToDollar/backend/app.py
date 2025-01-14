2ab14e79b68a2e1a45bee8ea05e819bf4a98bd2a
ghaluccas <gabsgit@gmail.com>
2025-01-14 15:46:37 -0300

    Todo : complete the '/convert' endpoint

--- RealToDollar/backend/app.py
+++ RealToDollar/backend/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from schemas import RealRequest , ConverterResponse
+from services import fetch_exchange
 
 main = FastAPI()
 
@@ -6,3 +8,10 @@ main = FastAPI()
 @main.get('/')
 def hello():
     return {'message': 'Hello word'}
+
+
+@main.get('/convert', response_class=ConverterResponse)
+async def convert(brl : RealRequest = 1.0):
+    exchnage_rate = await fetch_exchange
+    ...
+    
